2c8a7dc44b793b5572c1d631000c8048d28b4ee6
Jian Gong <jian@bettertx.com>
2025-03-19 22:22:52 -0400

    Added use case for vulncheck KEV and NVD+

--- cve_prioritizer/cve_prioritizer_tines.py
+++ cve_prioritizer/cve_prioritizer_tines.py
@@ -59,46 +59,35 @@ Throttle_msg = ''
 # )
 # logger = logging.getLogger(__name__)
 
-# argparse setup
-#@click.command()
-#@click.option('-a', '--api', type=str, help='Your API Key')
-#@click.option('-c', '--cve', type=str, help='Unique CVE-ID')
-#@click.option('-e', '--epss', type=float, default=0.2, help='EPSS threshold (Default 0.2)')
-#@click.option('-f', '--file', type=click.File('r'), help='TXT file with CVEs (One per Line)')
-#@click.option('-j', '--json_file', type=click.Path(), required=False, help='JSON output')
-#@click.option('-n', '--cvss', type=float, default=6.0, help='CVSS threshold (Default 6.0)')
-#@click.option('-o', '--output', type=click.File('w'), help='Output filename')
-#@click.option('-t', '--threads', type=int, default=100, help='Number of concurrent threads')
-#@click.option('-v', '--verbose', is_flag=True, help='Verbose mode')
-#@click.option('-l', '--list', help='Comma separated list of CVEs')
-#@click.option('-nc', '--no-color', is_flag=True, help='Disable Colored Output')
-#@click.option('-sa', '--set-api', is_flag=True, help='Save API keys')
-#@click.option('-vc', '--vulncheck', is_flag=True, help='Use NVD++ - Requires VulnCheck API')
-#@click.option('-vck', '--vulncheck_kev', is_flag=True, help='Use Vulncheck KEV - Requires VulnCheck API')
-#@click.option('--nessus', is_flag=True, help='Parse Nessus file')
-#@click.option('--openvas', is_flag=True, help='Parse OpenVAS file')
-#def main(api, cve, epss, file, cvss, output, threads, verbose, list, no_color, set_api, vulncheck, vulncheck_kev,
-#         json_file, nessus, openvas):
+# Main function. Receives input params from Tines story:
+# - nist_api_key
+# - vulncheck_api_key
+# - cve
+# - epss_threshold
+# - cvss_threshold
+#
+# TO DO:
+# - Add inputs for verbose, colored_output, write_file, and other values
 def main(input):
     # Global Arguments
-  #  color_enabled = not no_color
     throttle_msg = ''
 
-    # Set the NIST API and VULNCHECK API keys based on input from Tines' Python action
+    # Set params based on input from Tines' Python action
     nist_api = input["nist_api_key"]
     vulncheck_api = input["vulncheck_api_key"]
     cve = input["cve"]
+    epss_score = input["epss_threshold"]
+    cvss_score = input["cvss_threshold"]
 
     save_output = ''
     verbose = True
     colored_output = True
+    vc_kev = input["use_vulncheck_kev"]
+    nvd_plus = input["use_nvd_plus"]
 
     # By default, make the output verbose
     header = VERBOSE_HEADER
 
-  #  header = VERBOSE_HEADER if verbose else SIMPLE_HEADER
-  #  epss_threshold = epss
-  #  cvss_threshold = cvss
     sem = Semaphore(1)
 
     # Temporal lists
@@ -108,91 +97,47 @@ def main(input):
     # Tines is set up to pass only 1 CVE at a time to this function.
     cve_list.append(cve)
 
-    # Testing "cve" value
-    # return { "result": 'The cve is: ' + cve}
+    # Removed set_api code because it is not needed in the Tines story.
+    # Verbose header is set by default
+    # Tines will only input one cve at a time
+    # Removed print statement to display the logo
 
-  #  if set_api:
-  #      services = ['nist_nvd', 'vulncheck']
-  #      service = click.prompt("Please choose a service to set the API key",
-  #                             type=click.Choice(services, case_sensitive=False))
-  #      api_key = click.prompt(f"Enter the API key for {service}", hide_input=True)
-
-  #      if service == 'nist_nvd':
-  #          update_env_file('.env', 'NIST_API', api_key)
-  #      elif service == 'vulncheck':
-  #          update_env_file('.env', 'VULNCHECK_API', api_key)
-
-  #      click.echo(f"API key for {service} updated successfully.")
-  #  if verbose:
-  #      header = VERBOSE_HEADER
-
-  #  if cve:
-  #      cve_list.append(cve)
-  #  elif list:
-  #      cve_list = list.split(',')
-  #  elif file:
-  #      if nessus:
-  #          cve_list = parse_report(file, 'nessus')
-  #      elif openvas:
-  #          cve_list = parse_report(file, 'openvas')
-  #      else:
-  #          cve_list = [line.rstrip() for line in file]
-
-  #  if not api and not os.getenv('NIST_API') and not vulncheck:
-  #      if len(cve_list) > 75:
-  #          throttle_msg = 'Large number of CVEs detected, requests will be throttle to avoid API issues'
-  #          click.echo(LOGO + throttle_msg + '\n' +
-  #                     'Warning: Using this tool without specifying a NIST API may result in errors'
-  #                     + '\n\n' + header)
-  #      else:
-  #          click.echo(LOGO + 'Warning: Using this tool without specifying a NIST API may result in errors'
-  #                     + '\n\n' + header)
-  #  else:
-  #      print(LOGO + header)
-
-    #if output:
-    #    output.write("cve_id,priority,epss,cvss,cvss_version,cvss_severity,kev,ransomware,kev_source,cpe,vendor,"
-    #                 "product,vector" + "\n")
-
+    # Instantiate a blank list
     results = []
+
+    # Iterate through the CVEs. In Tines, this should only be 1 cve at a time.
     for cve in cve_list:
         throttle = 1
-  #      if len(cve_list) > 75 and not os.getenv('NIST_API') and not api and not vulncheck:
-  #          throttle = 6
-  #      if (vulncheck or vulncheck_kev) and (os.getenv('VULNCHECK_API') or api):
-  #          throttle = 0.25
-  #      elif (vulncheck or vulncheck_kev) and not os.getenv('VULNCHECK_API') and not api:
-  #          click.echo("VulnCheck requires an API key")
-  #          exit()
-  #      if not re.match(r'(CVE|cve-\d{4}-\d+$)', cve):
-  #          click.echo(f'{cve} Error: CVEs should be provided in the standard format CVE-0000-0000*')
-  #      else:
+
+        # Removed the throttle check code b/c this can be handled in Tines.
+
+        # Print for debug purposes
         print(cve)
 
+        # Use vulncheck KEV or NVD+ if specified in input
+        # Both are FALSE by default
         try:
             kev_source = 'CISA'
             print(kev_source)
-            #if vc_kev:
-            #    cve_result = vulncheck_check(cve_id, api, vc_kev)
+            if vc_kev:
+                cve_result = vulncheck_check(cve, vulncheck_api, vc_kev)
                 # exploited = vulncheck_kev(cve_id, api)[0]
-            #    exploited = cve_result.get('cisa_kev')
-            #    kev_source = 'VULNCHECK'
-            #elif nvd_plus:
-            #    cve_result = vulncheck_check(cve_id, api, vc_kev)
-            #    exploited = cve_result.get("cisa_kevs")
-            #else:
-            #    if 'vulncheck' in str(api).lower():
-            #        click.echo("Wrong API Key provided (VulnCheck)")
-            #        exit()
-            cve_result = nist_check(cve, nist_api)
-            print(cve_result)
-            exploited = cve_result.get("cisa_kev")
-            print(exploited)
+                exploited = cve_result.get('cisa_kev')
+                kev_source = 'VULNCHECK'
+            elif nvd_plus:
+                cve_result = vulncheck_check(cve, vulncheck_api, vc_kev)
+                exploited = cve_result.get("cisa_kevs")
+            else:
+                cve_result = nist_check(cve, nist_api)
+                print(cve_result)
+                exploited = cve_result.get("cisa_kev")
+                print(exploited)
+            
+            print(kev_source)
+
+            # Retrieve the EPSS result
             epss_result = epss_check(cve)
             print(epss_result)
-
-            cvss_score = 6
-            epss_score = 0.2
 
             try:
                 if exploited:
@@ -248,29 +193,10 @@ def main(input):
         finally:
             sem.release()
 
-    #for t in threads:
-    #    t.join()
-
-    #if json_file:
-    #    metadata = {
-    #        'generator': 'CVE Prioritizer',
-    #        'generation_date': datetime.now(timezone.utc).isoformat(),
-    #        'total_cves': len(cve_list),
-    #        'cvss_threshold': cvss_threshold,
-    #        'epss_threshold': epss_threshold,
-    #    }
-    #    output_data = {
-    #        'metadata': metadata,
-    #        'cves': results,
-    #    }
-    #    with open(json_file, 'w') as json_file:
-    #        json.dump(output_data, json_file, indent=4)
-
-
-if __name__ == '__main__':
-    main()
-
 # Check NIST NVD for the CVE
+# Modified code from CVE_Prioritizer, scripts/helpers.py
+# Added print statements for debugging 
+# Replaced click.echo with print statements to output to Script action logs
 def nist_check(cve_id, api_key):
     """
     Function collects NVD Data
@@ -360,6 +286,9 @@ def nist_check(cve_id, api_key):
     }
 
 # Collect EPSS Scores
+# Modified code from CVE_Prioritizer, scripts/helpers.py
+# Added print statements for debugging 
+# Replaced click.echo with print statements to output to Script action logs
 def epss_check(cve_id):
     """
     Function collects EPSS from FIRST.org
@@ -379,22 +308,177 @@ def epss_check(cve_id):
                            "percentile": int(float(cve.get("percentile")) * 100)}
                 return results
         else:
-            click.echo(f"{cve_id:<18}Not Found in EPSS.")
+            print(f"{cve_id:<18}Not Found in EPSS.")
             return {"epss": None, "percentile": None}
     except requests.exceptions.HTTPError as http_err:
-        click.echo(f"HTTP error occurred: {http_err}")
+        print(f"HTTP error occurred: {http_err}")
     except requests.exceptions.ConnectionError:
-        click.echo("Unable to connect to EPSS, check your Internet connection or try again")
+        print("Unable to connect to EPSS, check your Internet connection or try again")
     except requests.exceptions.Timeout:
-        click.echo("The request to EPSS timed out")
+        print("The request to EPSS timed out")
     except requests.exceptions.RequestException as req_err:
-        click.echo(f"An error occurred: {req_err}")
+        print(f"An error occurred: {req_err}")
     except ValueError as val_err:
-        click.echo(f"Error processing the response: {val_err}")
+        print(f"Error processing the response: {val_err}")
 
     return {"epss": None, "percentile": None}
 
+# Check Vulncheck NVD++
+# Modified code from CVE_Prioritizer, scripts/helpers.py
+# Added print statements for debugging 
+# Replaced click.echo with print statements to output to Script action logs
+def vulncheck_check(cve_id, api_key, kev_check):
+    """
+    Function collects VulnCheck NVD2 Data
+    """
+    try:
+        vulncheck_key = api_key
+        if not vulncheck_key:
+            print("VulnCheck requires an API key")
+            return {
+                "cvss_version": "",
+                "cvss_baseScore": "",
+                "cvss_severity": "",
+                "cisa_kev": "",
+                "ransomware": "",
+                "cpe": "",
+                "vector": ""
+            }
+
+        vulncheck_url = VULNCHECK_BASE_URL + f"?cve={cve_id}"
+
+        # Print the URL for debugging purposes
+        print(vulncheck_url)
+        
+        header = {"accept": "application/json"}
+        params = {"token": vulncheck_key}
+
+        vulncheck_response = requests.get(vulncheck_url, headers=header, params=params)
+        vulncheck_response.raise_for_status()
+
+        response_data = vulncheck_response.json()
+        if response_data.get("_meta", {}).get("total_documents", 0) > 0:
+            kev_data = requests.get(CISA_KEV_URL)
+            kev_data.raise_for_status()
+            kev_list = kev_data.json()
+
+            for unique_cve in response_data.get("data", []):
+                vc_kev = False
+                vc_used_by_ransomware = ''
+                if kev_check:
+                    vc_kev, vc_used_by_ransomware = vulncheck_kev(unique_cve.get('id'), api_key)
+                elif unique_cve.get("cisaExploitAdd"):
+                    vc_kev = True
+                    for entry in kev_list.get('vulnerabilities', []):
+                        if entry.get('cveID') == cve_id:
+                            vc_used_by_ransomware = str(entry.get('knownRansomwareCampaignUse')).upper()
+
+                cpe = unique_cve.get("configurations", [{}])[0].get("nodes", [{}])[0].get("cpeMatch", [{}])[0].get("criteria", 'cpe:2.3:::::::::::')
+
+                metrics = unique_cve.get("metrics", {})
+                for version in ["cvssMetricV31", "cvssMetricV30", "cvssMetricV2"]:
+                    if version in metrics:
+                        for metric in metrics[version]:
+                            return {
+                                "cvss_version": version.replace("cvssMetric", "CVSS "),
+                                "cvss_baseScore": float(metric.get("cvssData", {}).get("baseScore", 0)),
+                                "cvss_severity": metric.get("cvssData", {}).get("baseSeverity", ""),
+                                "cisa_kev": vc_kev,
+                                "ransomware": vc_used_by_ransomware,
+                                "cpe": cpe,
+                                "vector": metric.get("cvssData", {}).get("vectorString", "")
+                            }
+
+                if unique_cve.get("vulnStatus") == "Awaiting Analysis":
+                    print(f"{cve_id:<18}NIST Status: {unique_cve.get('vulnStatus')}")
+                    
+                    return {
+                        "cvss_version": "",
+                        "cvss_baseScore": "",
+                        "cvss_severity": "",
+                        "cisa_kev": "",
+                        "ransomware": "",
+                        "cpe": "",
+                        "vector": ""
+                    }
+        else:
+            print(f"{cve_id:<18}Not Found in VulnCheck.")
+            
+            return {
+                "cvss_version": "",
+                "cvss_baseScore": "",
+                "cvss_severity": "",
+                "cisa_kev": "",
+                "ransomware": "",
+                "cpe": "",
+                "vector": ""
+            }
+    except requests.exceptions.HTTPError:
+        print(f"{cve_id:<18}HTTP error occurred, check CVE ID or API Key")
+    except requests.exceptions.ConnectionError:
+        print("Unable to connect to VulnCheck, check your Internet connection or try again")
+    except requests.exceptions.Timeout:
+        print("The request to VulnCheck timed out")
+    except requests.exceptions.RequestException as req_err:
+        print(f"An error occurred: {req_err}")
+    except ValueError as val_err:
+        print(f"Error processing the response: {val_err}")
+
+    return {
+        "cvss_version": "",
+        "cvss_baseScore": "",
+        "cvss_severity": "",
+        "cisa_kev": "",
+        "ransomware": "",
+        "cpe": "",
+        "vector": ""
+    }
+
+# Checks VULNCHECK's KEV catalog for the CVE
+# Modified code from CVE_Prioritizer, scripts/helpers.py
+# Set vulncheck_key equal to api_key without conditional statement
+# Added print statements for debugging 
+# Replaced click.echo with print statements to output to Script action logs
+def vulncheck_kev(cve_id, api_key):
+    """
+    Check Vulncheck's KEV catalog
+    """
+
+    vc_exploited = False
+    vc_used_by_ransomware = False
+
+    try:
+        # Set the vulncheck API key to what is passed into the function
+        vulncheck_key = api_key
+        
+        # local variables
+        vulncheck_url = VULNCHECK_KEV_BASE_URL + f"?cve={cve_id}"
+        header = {"accept": "application/json"}
+        params = {"token": vulncheck_key}
+
+        # Check if API has been provided
+        if vulncheck_key:
+            vulncheck_response = requests.get(vulncheck_url, headers=header, params=params).json()
+
+            if vulncheck_response.get('data'):
+                vc_exploited = True
+                vc_used_by_ransomware = str(vulncheck_response.get('data')[0].get('knownRansomwareCampaignUse')).upper()
+                return vc_exploited, vc_used_by_ransomware
+            else:
+                return vc_exploited, vc_used_by_ransomware
+        else:
+            print("VulnCheck requires an API key")
+            exit()
+    except requests.exceptions.ConnectionError:
+        print(f"Unable to connect to VulnCheck, Check your Internet connection or try again")
+        return None, None
+
 # Function manages the outputs
+# Modified code from CVE_Prioritizer, scripts/helpers.py
+# Added return statement and replaced click.echo with print statements
+#
+# TO DO: 
+# - Update write to working_file.
 def print_and_write(working_file, cve_id, priority, epss, cvss_base_score, cvss_version, cvss_severity, kev, ransomware,
                     source, verbose, cpe, vector, no_color):
     color_priority = colored_print(priority)
@@ -402,25 +486,27 @@ def print_and_write(working_file, cve_id, priority, epss, cvss_base_score, cvss_
 
     if verbose:
         if no_color:
-            click.echo(
+            print(
                 f"{cve_id:<18}{color_priority:<22}{epss:<9}{cvss_base_score:<6}{cvss_version:<10}{cvss_severity:<10}"
                 f"{kev:<7}{ransomware:<12}{truncate_string(vendor, 15):<18}"
                 f"{truncate_string(product, 20):<23}{vector}")
         else:
-            click.echo(f"{cve_id:<18}{priority:<13}{epss:<9}{cvss_base_score:<6}{cvss_version:<10}{cvss_severity:<10}"
+            print(f"{cve_id:<18}{priority:<13}{epss:<9}{cvss_base_score:<6}{cvss_version:<10}{cvss_severity:<10}"
                        f"{kev:<7}{ransomware:<12}{truncate_string(vendor, 15):<18}"
                        f"{truncate_string(product, 20):<23}{vector}")
     else:
         if no_color:
-            click.echo(f"{cve_id:<18}{color_priority:<22}")
+            print(f"{cve_id:<18}{color_priority:<22}")
         else:
-            click.echo(f"{cve_id:<18}{priority:<13}")
+            print(f"{cve_id:<18}{priority:<13}")
     if working_file:
         working_file.write(f"{cve_id},{priority},{epss},{cvss_base_score},{cvss_version},{cvss_severity},"
                            f"{kev},{ransomware},{source},{cpe},{vendor},{product},{vector}\n")
 
     return {"cve_id": cve_id, "priority": priority, "epss": epss, "cvss_base_score": cvss_base_score, "cvss_version": cvss_version, "cvss_severity": cvss_severity, "kev": kev, "ransomware": ransomware, "vendor": truncate_string(vendor, 15), "product": truncate_string(product, 20), "vector": vector}
 
+# Prints outputs in color
+# Original code from CVE_Prioritizer, scripts/helpers.py
 def colored_print(priority):
     """
     Function used to handle colored print
@@ -437,6 +523,7 @@ def colored_print(priority):
         return colored(priority, 'green')
 
 # Truncate for printing
+# Original code from CVE_Prioritizer, scripts/helpers.py
 def truncate_string(input_string, max_length):
     """
     Truncates a string to a maximum length, appending an ellipsis if the string is too long.
@@ -447,6 +534,7 @@ def truncate_string(input_string, max_length):
         return input_string
 
 # Extract CVE product details
+# Original code from CVE_Prioritizer, scripts/helpers.py
 def parse_cpe(cpe_str):
     """
     Parses a CPE URI string and extracts the vendor, product, and version.
@@ -460,3 +548,6 @@ def parse_cpe(cpe_str):
     product = parts[4] if len(parts) > 3 else None
 
     return vendor, product
+
+if __name__ == '__main__':
+    main()
